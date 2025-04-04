8023eb292c52aa8ab12ddb07b56178a1136c3090
Zeeshan-eng-tech <zr0134146@gmail.com>
2025-04-04 01:38:25 -0700

    Update contact.component.css
    4-4-2025

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,16 +1,32 @@
 import { Component } from '@angular/core';
-import { ServiceService } from '../../service.service';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  contactForm: FormGroup;
 
-  constructor(private dataservice:ServiceService){
-    dataservice.getCheck();
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
   }
 
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+      alert('Message sent successfully!');
+      this.contactForm.reset();
+    } else {
+      alert('Please fill out the form correctly.');
+    }
+  }
 }
